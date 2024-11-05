260d57a3a9b14584746dea964a196593d775b0c5
GoBloxy <mostafa.eg.dz45@gmail.com>
2024-11-05 22:40:42 +0200

    Update social media links in profile configuration

--- src/config.ts
+++ src/config.ts
@@ -60,11 +60,11 @@ export const profileConfig: ProfileConfig = {
       icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
                                         // You will need to install the corresponding icon set if it's not already included
                                         // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
+      url: 'https://twitter.com/0xspy',
     },
     {
-      name: 'Steam',
-      icon: 'fa6-brands:steam',
+      name: 'YouTube',
+      icon: 'fa6-brands:youtube',
       url: 'https://store.steampowered.com',
     },
     {
