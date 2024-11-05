73b3d0fb908711286da1bc9f0694620575110ca7
GoBloxy <mostafa.eg.dz45@gmail.com>
2024-11-05 14:10:28 +0200

    a

--- src/config.ts
+++ src/config.ts
@@ -52,8 +52,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  name: '0xSpy',
+  bio: 'We\'s testing.',
   links: [
     {
       name: 'Twitter',
